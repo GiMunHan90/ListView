5185c8bf891744b0a0ebca73821c49b548f80cfb
gimun <gimunhan90@gmail.com>
2019-09-01 00:40:32 +0900

    listView

---
+++ app/src/main/java/com/gmail/listview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.gmail.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init(){
+        listView = findViewById(R.id.listView);
+
+        // 레이아웃 android.R.layout.simple_list_item_1 는 안드로이드가 기본적으로 제공하는 간단한 아이템 레이아웃
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        for (int i=0; i<20; i++){
+            adapter.add("한기문"+i);
+        }
+
+        // ListView에 각각의 아이템표시를 제어하는 Adapter를 설정
+        listView.setAdapter(adapter);
+
+        // 아이템을 [클릭]시의 이벤트 리스너를 등록
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView listView = (ListView) parent;
+                // TODO 아이템 클릭시에 구현할 내용은 여기에.
+                String item = (String) listView.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
